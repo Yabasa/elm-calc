25028a4f7b5f2ff4cfdd7e3160153b7ba99f1559
Yabasa <29189365+Yabasa@users.noreply.github.com>
2023-01-05 22:22:59 -0500

    First commit
    Basic working calculator with just addition and subtraction implemented.

---
+++ src/Calculator.elm
@@ -0,0 +1,273 @@
+module Calculator exposing (main)
+
+import Browser
+import Element exposing (Element, alignTop, centerX, column, el, layout, padding, row, spacing, text)
+import Element.Border as Border
+import Element.Input exposing (button)
+import Html exposing (Html)
+
+
+
+--TYPES
+
+
+type Operation
+    = Add
+    | Subtract
+    | Equal
+    | Clear
+
+
+type Mode
+    = InputNum1
+    | InputNum2
+    | Done
+
+
+type Msg
+    = NumPressed Int
+    | OperPressed Operation
+
+
+type alias Model =
+    { num1 : List Int
+    , num2 : List Int
+    , mode : Mode
+    , operation : Operation
+    , result : Maybe Float
+    }
+
+
+
+-- MAIN
+
+
+main : Program () Model Msg
+main =
+    Browser.sandbox
+        { init = initialModel
+        , view = view
+        , update = update
+        }
+
+
+
+-- MODEL
+
+
+initialModel : Model
+initialModel =
+    Model [] [] InputNum1 Clear Nothing
+
+
+
+-- UPDATE
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        NumPressed num ->
+            case model.mode of
+                InputNum1 ->
+                    { model | num1 = List.append model.num1 [ num ] }
+
+                InputNum2 ->
+                    { model | num2 = List.append model.num2 [ num ] }
+
+                Done ->
+                    model
+
+        OperPressed oper ->
+            case oper of
+                Equal ->
+                    if List.isEmpty model.num1 || List.isEmpty model.num2 then
+                        model
+
+                    else
+                        let
+                            num1 =
+                                Maybe.withDefault 0 <| flattenNums model.num1
+
+                            num2 =
+                                Maybe.withDefault 0 <| flattenNums model.num2
+                        in
+                        { model
+                            | mode = Done
+                            , result =
+                                case model.operation of
+                                    Add ->
+                                        Just (num1 + num2)
+
+                                    Subtract ->
+                                        Just (num1 - num2)
+
+                                    _ ->
+                                        Nothing
+                        }
+
+                Clear ->
+                    { model
+                        | num1 = []
+                        , num2 = []
+                        , mode = InputNum1
+                        , operation = Clear
+                        , result = Nothing
+                    }
+
+                Add ->
+                    if model.mode == Done then
+                        model
+
+                    else
+                        { model
+                            | operation = Add
+                            , mode = InputNum2
+                        }
+
+                Subtract ->
+                    if model.mode == Done then
+                        model
+
+                    else
+                        { model
+                            | operation = Subtract
+                            , mode = InputNum2
+                        }
+
+
+flattenNums : List Int -> Maybe Float
+flattenNums nums =
+    nums
+        |> List.map String.fromInt
+        |> List.foldr (++) ""
+        |> String.toFloat
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    layout []
+        (column [ spacing 20 ]
+            [ row [ spacing 50 ]
+                [ numberGrid
+                , actionButtons
+                ]
+            , resultsArea model
+            ]
+        )
+
+
+numberGrid : Element Msg
+numberGrid =
+    column [ spacing 10 ]
+        [ row [ spacing 10 ]
+            [ numberButton 1
+            , numberButton 2
+            , numberButton 3
+            ]
+        , row [ spacing 10 ]
+            [ numberButton 4
+            , numberButton 5
+            , numberButton 6
+            ]
+        , row [ spacing 10 ]
+            [ numberButton 7
+            , numberButton 8
+            , numberButton 9
+            ]
+        , el [ centerX ] <| numberButton 0
+        ]
+
+
+actionButtons : Element Msg
+actionButtons =
+    column [ alignTop, spacing 10 ]
+        [ calcButton (OperPressed Add) "+"
+        , calcButton (OperPressed Subtract) "-"
+        , calcButton (OperPressed Equal) "="
+        , calcButton (OperPressed Clear) "C"
+        ]
+
+
+resultsArea : Model -> Element Msg
+resultsArea model =
+    column []
+        [ row []
+            [ el [] <| text "Expression: "
+            , el [ centerX ] <| text <| compileExpression model
+            ]
+        , row []
+            [ el [] <| text "Result: "
+            , el [ centerX ] <|
+                text
+                    (if model.result == Nothing then
+                        ""
+
+                     else
+                        String.fromFloat <| Maybe.withDefault 0 model.result
+                    )
+            ]
+        ]
+
+
+operationAsString : Operation -> String
+operationAsString oper =
+    case oper of
+        Add ->
+            " + "
+
+        Subtract ->
+            " - "
+
+        Equal ->
+            ""
+
+        Clear ->
+            ""
+
+
+calcButton : Msg -> String -> Element Msg
+calcButton msg labelText =
+    button
+        [ Border.width 1
+        , padding 5
+        , Element.width <| Element.px 70
+        , Element.height <| Element.px 70
+        , Border.rounded 5
+        ]
+        { onPress = Just msg
+        , label = el [ centerX ] (text labelText)
+        }
+
+
+numberButton : Int -> Element Msg
+numberButton num =
+    calcButton (NumPressed num) (String.fromInt num)
+
+
+compileExpression : Model -> String
+compileExpression model =
+    let
+        num1 =
+            if flattenNums model.num1 == Nothing then
+                ""
+
+            else
+                String.fromFloat <| Maybe.withDefault 0 <| flattenNums model.num1
+
+        num2 =
+            if flattenNums model.num2 == Nothing then
+                ""
+
+            else
+                String.fromFloat <| Maybe.withDefault 0 <| flattenNums model.num2
+
+        oper =
+            operationAsString <| model.operation
+    in
+    num1 ++ oper ++ num2
+
